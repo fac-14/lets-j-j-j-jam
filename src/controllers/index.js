040e797fea1f533aaa20017e1cc13fa8d5da2b1c
Joe Friel <joseph.s.friel@gmail.com>
2018-08-22 15:44:11 +0100

    Build express engine, start routes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const home = require("./home");
+const topics = require("./topics");
+const articles = require("./articles");
+const addArticle = require("./add-article");
 
 // create route handler
 router.get("/", home.get);
+router.get("/topics", topics.get);
+router.get("/topics/articles", articles.get);
+router.get("/add-article", addArticle.get);
 
 module.exports = router;
