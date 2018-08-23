e4f0ab18b0c86afecc71f9c569f76a7a5ec7e4a6
MissArray <33687551+MissArray@users.noreply.github.com>
2018-08-23 11:40:07 +0100

    Add getTopics query

--- src/controllers/topic.js
+++ src/controllers/topic.js
@@ -1,3 +1,3 @@
 exports.get = (req, res) => {
-  res.render("topic");
+  res.render("topic", {query.getTopics});
 };
--- src/model/getTopics.js
+++ src/model/getTopics.js
@@ -1,3 +1,16 @@
 const db = require('../../db/db_connection');
 
-// Function to get all topics
+const getTopics = ( ) => {
+    db.query(
+    `SELECT * FROM topics`
+    )
+    .then(res => console.log('Queried topics', res))
+    .catch(err => console.error('error!', err))
+}
+
+getTopics();
+
+module.exports = getTopics;
+// module.exports = db.query(
+//     `SELECT * FROM topics` 
+// );
